83a4d0dd14289e869b7aedec3e89222f3890da39
Lingfeng Fu <2871618714@qq.com>
2024-01-26 20:45:25 +0800

    jokes bank

---
+++ Assets/Scripts/So/JokesBankSo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace So
+{
+    [CreateAssetMenu(fileName = "jokes", menuName = "ScriptableObjects/JokesBank")]
+    public class JokesBankSo : ScriptableObject
+    {
+        public List<string> jokes = new();
+    }
+}
