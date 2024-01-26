3070656fdf3ab9de6021360a7132b3f6eb739ee6
Lingfeng Fu <2871618714@qq.com>
2024-01-26 16:13:11 +0800

    player control

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerControl : MonoBehaviour
+{
+    private CharacterController _controller;
+    public float speed = 5.0f;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            HoldEars();
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            ReleaseEars();
+        if (Input.GetKeyDown(KeyCode.Space))
+            CloseEyes();
+        if (Input.GetKeyUp(KeyCode.Space))
+            OpenEyes();
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 direction = new(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        if (!Mathf.Approximately(direction.sqrMagnitude, 0.0f))
+        {
+            _controller.SimpleMove(speed * direction);
+        }
+    }
+
+    private void HoldEars()
+    {
+        Debug.Log(nameof(HoldEars));
+    }
+
+    private void ReleaseEars()
+    {
+        Debug.Log(nameof(ReleaseEars));
+    }
+
+    private void CloseEyes()
+    {
+        Debug.Log(nameof(CloseEyes));
+    }
+
+    private void OpenEyes()
+    {
+        Debug.Log(nameof(OpenEyes));
+    }
+}
