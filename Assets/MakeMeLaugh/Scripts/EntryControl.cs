f187eb57aa5b97da286042a5467eb05a5abffbf5
Lingfeng Fu <2871618714@qq.com>
2024-01-28 06:10:01 +0800

    entry sound

--- Assets/MakeMeLaugh/Scripts/EntryControl.cs
+++ Assets/MakeMeLaugh/Scripts/EntryControl.cs
@@ -1,9 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 
 namespace MakeMeLaugh.Scripts
 {
+    [RequireComponent(typeof(AudioSource))]
     public class EntryControl : MonoBehaviour
     {
+        private AudioSource _audio;
+        [SerializeField] private AudioResource _huh;
+        [SerializeField] private AudioResource _hmmm;
+
+        private void Start()
+        {
+            _audio = GetComponent<AudioSource>();
+        }
+
+        public void OnPointerEnterPlayButton()
+        {
+            if (!_audio.isPlaying)
+            {
+                _audio.resource = _huh;
+                _audio.Play();
+            }
+        }
+
+        public void OnPointerEnterQuitButton()
+        {
+            if (!_audio.isPlaying)
+            {
+                _audio.resource = _hmmm;
+                _audio.Play();
+            }
+        }
+
         public void Play()
         {
             GameControl.OpenBattle();
